1ad9a67327b3d381a0d1bdd4a8fdaf90f4d5b110
maotterson <otterson.2@gmail.com>
2022-03-28 22:37:54 -0400

    add signalR microservice project

---
+++ GameLobbySignalRTemplate/SignalR/Hubs/LobbyHub.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.SignalR;
+namespace GameLobbySignalRTemplate.SignalR.Hubs
+{
+    public class LobbyHub : Hub
+    {
+        public LobbyHub()
+        {
+            
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            string userId = Context.ConnectionId;
+            await Clients.All.SendAsync("UserConnected", userId);
+            await base.OnConnectedAsync();
+        }
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            string userId = Context.ConnectionId;
+            await Clients.All.SendAsync("UserDisconnected", userId);
+            await base.OnDisconnectedAsync(exception);
+        }
+    }
+}
---
+++ GameLobbySignalRTemplate/SignalR/Program.cs
@@ -0,0 +1,35 @@
+using GameLobbySignalRTemplate.SignalR.Hubs;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.ResponseCompression;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddServerSideBlazor();
+builder.Services.AddSignalR();
+builder.Services.AddResponseCompression(opts =>
+{
+    opts.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(
+        new[] { "application/octet-stream" });
+});
+
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.MapBlazorHub();
+app.MapHub<LobbyHub>("/lobbyhub");
+
+app.Run();
